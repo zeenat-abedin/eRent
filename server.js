f91e44765b85849c2651b0b07452c593de211506
zeenat-abedin-au3 <zeenu.23@gmail.com>
2020-02-09 10:59:11 +0530

    db setup done

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const experss = require('express')
 
 const app = experss()
+const sequelize = require('./config/db')
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+
+// DB Connection
+sequelize.authenticate()
+  .then(function (err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err.message);
+  });
 
 
 // Start Server
